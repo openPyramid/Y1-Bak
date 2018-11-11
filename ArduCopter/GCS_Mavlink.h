ea3f13f1750a18aa34e50f5fa614a67fafe551e6
goredhawk <goredhawk@163.com>
2018-11-11 10:02:46 +0800

    BeaconParams: add beaconParams.h and Add Beacon message handler, Beacon params, special point, command_long etc.

--- ArduCopter/GCS_Mavlink.h
+++ ArduCopter/GCS_Mavlink.h
@@ -35,6 +35,17 @@ protected:
 
     void send_position_target_global_int() override;
 
+	/************** xiao deng add Command_Long ***********/
+    MAV_RESULT handle_command_get_point_ab(const mavlink_command_long_t &packet) override;
+    MAV_RESULT handle_command_clear_point_ab() override;
+    MAV_RESULT handle_command_start_work(const mavlink_command_long_t &packet) override;
+    MAV_RESULT handle_command_pause_work() override;
+    MAV_RESULT handle_command_finish_work() override;
+	/************** xiao deng add done ***************/
+
+	uint32_t setBeaconParams() override;
+	uint32_t setSpecialPointInfo() override;
+
 private:
 
     void handleMessage(mavlink_message_t * msg) override;
